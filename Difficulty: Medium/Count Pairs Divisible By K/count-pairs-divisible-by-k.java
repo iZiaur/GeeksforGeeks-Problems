59d6c837c8b33605dd1d3db7197764fab045c06a
Md Ziaur Rahman <iziaurofficial@gmail.com>
2026-07-09 09:34:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Pairs Divisible By K/count-pairs-divisible-by-k.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int countKdivPairs(int[] arr, int k) {
+        int[] freq = new int[k];
+        int count = 0;
+
+        for (int num : arr) {
+            int rem = num % k;
+
+            
+            if (rem < 0) rem += k;
+
+            int need = (k - rem) % k;
+
+            count += freq[need];
+
+            freq[rem]++;
+        }
+
+        return count;
+    }
+}
