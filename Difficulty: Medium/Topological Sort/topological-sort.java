1f87d3623926f4ee7873e4b6bb4192dbb9449887
Md Ziaur Rahman <iziaurofficial@gmail.com>
2026-09-17 20:18:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological Sort/topological-sort.java
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -1,17 +1,7 @@
 class Solution {
-    public void dfsutil(int start,ArrayList<ArrayList<Integer>> adj,boolean visited[],Stack<Integer> st){
-        visited[start]=true;
-        ArrayList<Integer>temp=adj.get(start);
-        for(int i=0;i<temp.size();i++){
-            if(!visited[temp.get(i)]){
-                dfsutil(temp.get(i),adj,visited,st);
-            }
-        }
-        
-        st.push(start);
-    }
+   
     public ArrayList<Integer> topoSort(int V, int[][] edges) {
-        // code here
+        // code here using kahs algorithm
     ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
     for(int i=0;i<V;i++){
         adj.add(new ArrayList<>());
@@ -23,17 +13,32 @@ class Solution {
         adj.get(u).add(v);
     }
     
-    boolean visited[]=new boolean[V];
-    Stack<Integer>st=new Stack<>();
-    for(int i=0;i<V;i++){
-        if(!visited[i]){
-            dfsutil(i,adj,visited,st);
+    int[] indegree=new int[V];
+    for(int i=0;i<adj.size();i++){
+        ArrayList<Integer>temp=adj.get(i);
+        for(int j=0;j<temp.size();j++){
+            int num=temp.get(j);
+            indegree[num]++;
         }
     }
     
-    ArrayList<Integer> ans=new ArrayList<>();
-    while(!st.isEmpty()){
-        ans.add(st.pop());
+    Queue<Integer> q=new LinkedList<>();
+    for(int i=0;i<indegree.length;i++){
+        if(indegree[i]==0){
+            q.add(i);
+        }
+    }
+    ArrayList<Integer>ans=new ArrayList<>();
+    while(!q.isEmpty()){
+        Integer curr=q.remove();
+        ans.add(curr);
+        ArrayList<Integer>temp=adj.get(curr);
+        for(int i=0;i<temp.size();i++){
+            indegree[temp.get(i)]--;
+            if(indegree[temp.get(i)]==0){
+                q.add(temp.get(i));
+            }
+        }
     }
     
     return ans;
